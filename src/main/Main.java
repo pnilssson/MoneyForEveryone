74ca524d70937fed0f7abfc1d2fbfd824492fdb2
Wictor Sundström <wictor.sundstrom@hotmail.com>
2020-01-20 11:56:45 +0100

    test folders

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,10 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
+        testTest(10);
+    }
 
+    public static int testTest(int input) {
+        return input * input;
     }
 }
---
+++ src/test/unit_account/LoginTest.java
@@ -0,0 +1,8 @@
+package test.unit_account;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+}
