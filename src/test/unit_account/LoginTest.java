0363d502e1e5b522c126545153d10eda45242760
pnilssson <p.nilsson95@hotmail.com>
2020-01-27 13:51:44 +0100

    Updated tests to use department aswell

--- src/test/unit_account/LoginTest.java
+++ src/test/unit_account/LoginTest.java
@@ -2,9 +2,10 @@ package test.unit_account;
 
 import main.GetInputs;
 import main.Login;
-import main.Role;
 import main.account.Account;
 import main.account.AccountList;
+import main.enums.Department;
+import main.enums.Role;
 import main.user.UserModel;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,7 +13,7 @@ import org.junit.Test;
 public class LoginTest {
     Login login = new Login();
     GetInputs getInput = new GetInputs();
-    UserModel user = new UserModel(10, 0, Role.USER, "user1", "qwerty");
+    UserModel user = new UserModel(10, 0, Role.USER, Department.DEVELOPER, "user1", "qwerty");
     Account account = user;
 
     @Test
