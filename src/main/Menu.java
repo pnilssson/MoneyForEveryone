09241110b8444a96d507e661cad5468b54f02242
pnilssson <p.nilsson95@hotmail.com>
2020-01-27 09:56:20 +0100

    Fix for double input on first menu

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -3,15 +3,13 @@ package main;
 import java.util.Scanner;
 
 public class Menu {
-    Scanner scan = new Scanner(System.in);
-
     public void startMenu() {
         boolean choice = true;
         while(choice) {
             System.out.println("1) Login");
             System.out.println("0) Exit");
             System.out.print("Your choice: ");
-            choice = menuChoice(scan.next());
+            choice = menuChoice(new Scanner(System.in).next());
         }
     }
 
