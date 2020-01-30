7ac012ea31629df0d7a75628b3d163251c1a5c98
Wictor Sundström <wictor.sundstrom@hotmail.com>
2020-01-30 17:56:37 +0100

    added accept and decline tests

--- src/test/integration/DepartmentTest.java
+++ src/test/integration/DepartmentTest.java
@@ -1,6 +1,7 @@
 package test.integration;
 
 import main.admin.AdminController;
+import main.enums.Department;
 import main.enums.Role;
 import main.user.UserController;
 import main.user.UserModel;
@@ -9,19 +10,64 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DepartmentTest {
-    UserModel user = new UserModel(10, 0, Role.USER, main.enums.Department.DEVELOPER, "testuser", "testpassword");
+    UserModel user = new UserModel(10, 0, Role.USER, Department.DEVELOPER, "testuser", "testpassword");
     UserController userController = new UserController();
     AdminController adminController = new AdminController();
 
     @Test
     public void testRequestDepartmentAndApply() {
-        Assert.assertEquals("User department is incorrect", main.enums.Department.DEVELOPER, user.getDepartment());
-        userController.submitDepartmentChange(user, main.enums.Department.HR);
-        Assert.assertEquals("Requested department not changed", main.enums.Department.HR, user.getRequestedNewDepartment());
+        Assert.assertEquals("User department is incorrect", Department.DEVELOPER, user.getDepartment());
 
+        userController.submitDepartmentChange(user, Department.HR);
+        Assert.assertEquals("Requested department not changed", Department.HR, user.getRequestedNewDepartment());
         AccountList.accountArrayList.add(user);
-        user.setRequestedNewDepartment(main.enums.Department.CEO);
+
         Assert.assertTrue("Incorrect any Requests returned with new department requested", adminController.displayAllRequests());
 
+        adminController.acceptDepartmentChange(user);
+        Assert.assertEquals("Department is not changed", Department.HR, user.getDepartment());
+    }
+
+    @Test
+    public void testRequestDepartmentAndDecline() {
+        Assert.assertEquals("User department is incorrect", Department.DEVELOPER, user.getDepartment());
+
+        userController.submitDepartmentChange(user, Department.HR);
+        Assert.assertEquals("Requested department not changed", Department.HR, user.getRequestedNewDepartment());
+        AccountList.accountArrayList.add(user);
+
+        Assert.assertTrue("Incorrect any Requests returned with new department requested", adminController.displayAllRequests());
+
+        adminController.declineDepartmentChange(user);
+        Assert.assertEquals("Department is not changed", Department.DEVELOPER, user.getDepartment());
+    }
+
+
+    @Test
+    public void testRequestSalaryAndApply() {
+        Assert.assertEquals("User salary is incorrect", 10, user.getSalary());
+
+        userController.submitSalaryChange(user, 20);
+        Assert.assertEquals("Requested salary not changed", 20, user.getRequestedSalary());
+        AccountList.accountArrayList.add(user);
+
+        Assert.assertTrue("Incorrect any Requests returned with new salary requested", adminController.displayAllRequests());
+
+        adminController.acceptSalaryChange(user);
+        Assert.assertEquals("Salary is not changed", 20, user.getSalary());
+    }
+
+    @Test
+    public void testRequestSalaryAndDecline() {
+        Assert.assertEquals("User salary is incorrect", 10, user.getSalary());
+
+        userController.submitSalaryChange(user, 20);
+        Assert.assertEquals("Requested salary not changed", 20, user.getRequestedSalary());
+        AccountList.accountArrayList.add(user);
+
+        Assert.assertTrue("Incorrect any Requests returned with new salary requested", adminController.displayAllRequests());
+
+        adminController.declineSalaryChange(user);
+        Assert.assertEquals("Salary is not changed", 10, user.getSalary());
     }
 }
